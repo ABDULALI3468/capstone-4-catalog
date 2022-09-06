415d34e19ef4a3d4cac7780dc52010c685aa9e41
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-09-06 16:36:00 +0500

    Created necessary function for storing data in files regarding books and labels.

--- cli.rb
+++ cli.rb
@@ -1,19 +1,50 @@
+require_relative 'store_data'
 class Cli
+  include DataStore
+
   def initialize
+    @books = load('./Data/books.json')
+    @labels = load('./Data/labels.json')
     puts 'Welcome to "Catalog Of My Things" app!'
     puts
   end
 
   def print_main_menu
     puts 'Please choose and option by entering a number:'
-    puts '1 - List all items'
-    puts '2 - Print item by ID'
+    puts '1 - List all books'
+    puts '2 - List all labels'
+    puts '3 - Add a book'
     puts 'Q - Exit'
 
     read_menu_input
   end
 
-  def read_menu_input(range = (1..2))
+  def create_book
+    print 'Who is the Publisher: '
+    publisher = gets.chomp
+    print 'What is the cover state: '
+    cover_state = gets.chomp
+    print 'When the book was published: '
+    published_date = gets.chomp
+    write_books(publisher, cover_state, published_date, @books)
+    @books = load('./Data/books.json')
+    print 'Book created successfully'
+  end
+
+  def list_books
+    @books.each_with_index do |book, index|
+      puts("(#{index})- Publisher: #{book['publisher']} , Cover State: #{book['cover_state']} , Published Date: #{book['publish_date']}")
+    end
+  end
+
+  def list_labels
+    @labels.each_with_index do |label, index|
+      puts("(#{index})- Title: #{label['title']} , Color: #{label['color']}")
+    end
+  end
+
+
+  def read_menu_input(range = (1..3))
     input = gets.chomp.to_i
     input = nil unless range.include?(input)
 
--- main.rb
+++ main.rb
@@ -6,9 +6,14 @@ def main
     choice = cli.print_main_menu
     case choice
     when 1
-      puts 'Listing all items'
+      puts 'Listing all books'
+      cli.list_books
     when 2
-      puts 'Getting items by ID'
+      puts 'List all labels'
+      cli.list_labels
+    when 3
+      puts 'Add a book'
+      cli.create_book
     else
       puts 'App quitting gracefully..'
       break
