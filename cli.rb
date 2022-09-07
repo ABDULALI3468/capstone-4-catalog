93b67a9d8cfc9a2a72ddc42b60e566e90be5ee47
BHDHakim <hakim.bhd@gmail.com>
2022-09-07 16:34:31 +0300

    Add CLI options

--- cli.rb
+++ cli.rb
@@ -8,12 +8,15 @@ class Cli
     puts 'Please choose and option by entering a number:'
     puts '1 - List all items'
     puts '2 - Print item by ID'
+    puts '4 - List of games'
+    puts '5 - List all authors'
+    puts '6 - Add a game'
     puts 'Q - Exit'
 
     read_menu_input
   end
 
-  def read_menu_input(range = (1..2))
+  def read_menu_input(range = (1..6))
     input = gets.chomp.to_i
     input = nil unless range.include?(input)
 
