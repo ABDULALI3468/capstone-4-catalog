3802861015d98557f4e707e672f0dc223cfb48e6
Steve <sntakirutimana72@gmail.com>
2022-09-07 12:51:32 +0200

    Generate a schema.sql file from project UML diagram

---
+++ schema.sql
@@ -0,0 +1,41 @@
+create table genres(
+    id   int generated always as identity,
+    name varchar(255),
+    primary key(id)
+);
+
+create table authors(
+    id         int generated always as identity,
+    first_name varchar(255),
+    last_name  varchar(255),
+    primary key(id)
+);
+
+create table sources(
+    id   int generated always as identity,
+    name varchar(255),
+    primary key(id)
+);
+
+create table labels(
+    id    int generated always as identity,
+    title varchar(255),
+    color varchar(255),
+    primary key(id)
+);
+
+create table music_albums(
+    id           int generated always as identity,
+    genre_id     int,
+    author_id    int,
+    source_id    int,
+    label_id     int,
+    publish_date date,
+    on_spotify   boolean,
+    archived     boolean,
+    primary key(id),
+    foreign key(genre_id) references genres(id),
+    foreign key(author_id) references authors(id),
+    foreign key(source_id) references sources(id),
+    foreign key(label_id) references labels(id)
+);
